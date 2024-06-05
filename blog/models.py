09f37cd3d3187df9044d0e21b5e35f520f7ebd78
Annan Deriba <annanderiba@Annans-MacBook-Air.local>
2024-06-05 15:45:31 +0300

    Finalized a basic structure for the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField() # is not going to be the full body of the blog post but a short starter display
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at',]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def frontpage(request):
-    return render(request, "core/front_page.html", {})
+    posts = Post.objects.all()
+    return render(request, "core/front_page.html", {'posts': posts})
 
 
 def about(request):
