2c4101dca4c4528a4b55cba47353f1da2744464b
Annan Deriba <annanderiba@Annans-MacBook-Air.local>
2024-06-05 17:44:05 +0300

    Added a comment feature for blog posts

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,14 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-created_at',]
+
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Post
+from .models import Post, Comment
 
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        message = request.POST.get('message', '')
+
+        if name and message:
+            Comment.objects.create(name=name, message=message, post=post)
+
+            return redirect(f"/blog/{slug}/#comments")
+        
 
     return render(request, 'blog/detail.html', {"post": post})
